d5e1ee08699cf8daa95e15f6ece710083030c3e5
psychobunny <psycho.bunny@hotmail.com>
2012-11-29 17:26:07 -0500

    fixed bug by updating regex to non-greedy

--- plugins/base/shape.js
+++ plugins/base/shape.js
@@ -68,8 +68,8 @@ function Draggable(element) {
 	document.body.addEventListener('mouseup', function(ev) {
 		console.log('test');
 		Selector.pause = false;		
-		currentElement.rawcss = currentElement.rawcss.replace(/left(.*)px/g, 'left: ' + currentElement.style.left);
-		currentElement.rawcss = currentElement.rawcss.replace(/top(.*)px/g, 'top: ' + currentElement.style.top);
+		currentElement.rawcss = currentElement.rawcss.replace(/left(.*?)px/g, 'left: ' + currentElement.style.left);
+		currentElement.rawcss = currentElement.rawcss.replace(/top(.*?)px/g, 'top: ' + currentElement.style.top);
 
 		document.getElementById('container').removeEventListener('mousemove', ref);
 		currentElement = null;
